f668f3d588a1a1caf54ef06d1907f2e4e3bc93a9
Joseph Sayegh <joesayegh@gmail.com>
2022-02-07 17:03:05 +0000

    First commit

---
+++ src/constants/images.js
@@ -0,0 +1,55 @@
+import bg from "../assets/bg.jpg";
+import chef from "../assets/chef.png";
+import G from "../assets/G.png";
+import Gsvg from "../assets/G.svg";
+import gallery01 from "../assets/gallery01.png";
+import gallery02 from "../assets/gallery02.png";
+import gallery03 from "../assets/gallery03.png";
+import gallery04 from "../assets/gallery04.png";
+import knife from "../assets/knife.png";
+import logo from "../assets/logo.svg";
+import menu from "../assets/menu.png";
+import overlaybg from "../assets/overlaybg.png";
+import spoon from "../assets/spoon.svg";
+import welcome from "../assets/welcome.png";
+import findus from "../assets/findus.png";
+import laurels from "../assets/laurels.png";
+import award01 from "../assets/award01.png";
+import award02 from "../assets/award02.png";
+import award03 from "../assets/award03.png";
+import award05 from "../assets/award05.png";
+import sign from "../assets/sign.png";
+import signsvg from "../assets/sign.svg";
+import quote from "../assets/quote.png";
+import quotesvg from "../assets/quote.svg";
+import gericht from "../assets/gericht.png";
+import gerichtsvg from "../assets/gericht.svg";
+
+export default {
+	bg,
+	chef,
+	G,
+	Gsvg,
+	gallery01,
+	gallery02,
+	gallery03,
+	gallery04,
+	knife,
+	logo,
+	menu,
+	overlaybg,
+	spoon,
+	welcome,
+	findus,
+	laurels,
+	award01,
+	award02,
+	award03,
+	award05,
+	sign,
+	signsvg,
+	quote,
+	quotesvg,
+	gericht,
+	gerichtsvg,
+};
