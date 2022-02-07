30dd5b287dc1fbf9ec2428305593d97b7365dee4
Joseph Sayegh <joesayegh@gmail.com>
2022-02-07 18:11:03 +0000

    fixes bug where menu links weren't scrolling to anchors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
-
 import images from "../../constants/images";
 import "./Navbar.css";
 
@@ -10,28 +9,28 @@ const Navbar = () => {
 	return (
 		<nav className="app__navbar">
 			<div className="app__navbar-logo">
-				<img src={images.gerichtsvg} alt="app logo" />
+				<img src={images.gerichtsvg} alt="app__logo" />
 			</div>
 			<ul className="app__navbar-links">
 				<li className="p__opensans">
-					<a herf="#home">Home</a>
+					<a href="#home">Home</a>
 				</li>
 				<li className="p__opensans">
-					<a herf="#about">About</a>
+					<a href="#about">About</a>
 				</li>
 				<li className="p__opensans">
-					<a herf="#menu">Menu</a>
+					<a href="#menu">Menu</a>
 				</li>
 				<li className="p__opensans">
-					<a herf="#awards">Awards</a>
+					<a href="#awards">Awards</a>
 				</li>
 				<li className="p__opensans">
-					<a herf="#contact">Contact</a>
+					<a href="#contact">Contact</a>
 				</li>
 			</ul>
 			<div className="app__navbar-login">
 				<a href="#login" className="p__opensans">
-					Log In / Register
+					Log In / Registration
 				</a>
 				<div />
 				<a href="/" className="p__opensans">
@@ -40,25 +39,34 @@ const Navbar = () => {
 			</div>
 			<div className="app__navbar-smallscreen">
 				<GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
-
 				{toggleMenu && (
 					<div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
 						<MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
 						<ul className="app__navbar-smallscreen_links">
-							<li className="p__opensans">
-								<a herf="#home">Home</a>
+							<li>
+								<a href="#home" onClick={() => setToggleMenu(false)}>
+									Home
+								</a>
 							</li>
-							<li className="p__opensans">
-								<a herf="#about">About</a>
+							<li>
+								<a href="#about" onClick={() => setToggleMenu(false)}>
+									About
+								</a>
 							</li>
-							<li className="p__opensans">
-								<a herf="#menu">Menu</a>
+							<li>
+								<a href="#menu" onClick={() => setToggleMenu(false)}>
+									Menu
+								</a>
 							</li>
-							<li className="p__opensans">
-								<a herf="#awards">Awards</a>
+							<li>
+								<a href="#awards" onClick={() => setToggleMenu(false)}>
+									Awards
+								</a>
 							</li>
-							<li className="p__opensans">
-								<a herf="#contact">Contact</a>
+							<li>
+								<a href="#contact" onClick={() => setToggleMenu(false)}>
+									Contact
+								</a>
 							</li>
 						</ul>
 					</div>
